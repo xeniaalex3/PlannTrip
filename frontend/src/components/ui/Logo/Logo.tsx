270d1fb870cfb4edd5aa32cbdb68ceaa77e7636f
[xeniaalex3] <[xeniaalex2@gmail.com]>
2025-05-05 19:21:59 +0200

    ✨ update logo component and integrate new logo image; adjust index.html title and import styles correctly

---
+++ frontend/src/components/ui/Logo/Logo.tsx
@@ -0,0 +1,16 @@
+import logo from '../../../assets/images/Vector.png'
+
+export default function Logo() {
+  return (
+    <div className="flex items-center justify-center gap-2">
+      <img
+        src={logo}
+        alt="picture of logo"
+        className="h-9 w-9 object-contain"
+      />
+      <h1 className="text-zinc-50 font-semibold text-3xl leading-none">
+        plannTrip
+      </h1>
+    </div>
+  )
+}
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,16 @@
 import SearchForm from '../components/SearchForm/SearchForm'
+import Logo from '../components/ui/Logo/Logo'
 
 export default function Home() {
   return (
     <div className="max-w-3x px-6 text-center space-y-10">
+      <div className='space-y-5'>
+      <Logo />
       <p className="text-zinc-300 text-lg">
         Invitez vos amis et planifiez votre prochain voyage !
       </p>
+      </div>
+      
       <SearchForm />
       <p className="text-zinc-500 text-sm">
         En planifiant votre voyage par l'intermédiaire de PlannTrip, vous
