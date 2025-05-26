62d36a092208d2d48c1682b8354e21706f601b5e
[xeniaalex3] <[xeniaalex2@gmail.com]>
2025-05-26 17:15:34 +0200

    refactor InputWrapper and Home components; streamline input handling and improve state management

--- frontend/src/@types/form.ts
+++ frontend/src/@types/form.ts
@@ -2,7 +2,8 @@ import { type InputHTMLAttributes } from "react";
 
 export interface InputWrapperProps extends InputHTMLAttributes<HTMLInputElement> {
   placeholder: string;
-  type: "text" | "date";
+  type: "text" | "date" | "email";
   className?: string;
   disabled?: boolean;
+  name?: string;
 }
--- frontend/src/components/form/InputWrapper/InputWrapper.tsx
+++ frontend/src/components/form/InputWrapper/InputWrapper.tsx
@@ -1,12 +1,13 @@
 import type { InputWrapperProps } from "../../../@types/form"
 
-export default function InputWrapper({ placeholder, type, className, disabled}: InputWrapperProps){
+export default function InputWrapper({ placeholder, type, name, className, disabled}: InputWrapperProps){
   return(
     <input 
     type={type} 
     placeholder={placeholder}
     className={className}
     disabled={disabled}
+    name={name}
     />
   )
 }
