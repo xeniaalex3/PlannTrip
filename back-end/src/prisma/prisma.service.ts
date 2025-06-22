b0af0c486e6133a38b4050ea83db7c2d1660a3ec
[xeniaalex3] <[xeniaalex2@gmail.com]>
2025-06-22 17:58:16 +0200

    feat: implement Prisma with PostgreSQL, add trip-related entities, and update service/controller logic

--- back-end/src/prisma/prisma.service.ts
+++ back-end/src/prisma/prisma.service.ts
@@ -1,5 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {}
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit(): Promise<void> {
+    await this.$connect();
+  }
+
+  async onModuleDestroy(): Promise<void> {
+    await this.$disconnect();
+  }
+}
