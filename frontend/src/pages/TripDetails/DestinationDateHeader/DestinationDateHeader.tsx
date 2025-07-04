88d6872a3a796735e4602d11a6203efbe2621146
[xeniaalex3] <[xeniaalex2@gmail.com]>
2025-07-04 17:06:31 +0200

    ✨ integrate TripContext into main app structure; update DestinationDateHeader to display trip location and formatted dates

--- frontend/src/pages/TripDetails/DestinationDateHeader/DestinationDateHeader.tsx
+++ frontend/src/pages/TripDetails/DestinationDateHeader/DestinationDateHeader.tsx
@@ -1,17 +1,22 @@
 import { Calendar, MapPin, Settings2 } from 'lucide-react'
 import CustomButton from '../../../components/ui/Button/CustomButton/CustomButton'
+import { useTrip } from '../../../context/TripContext'
+import { formatDateRange } from '../../../utils/date';
 
 export default function DestinationDateHeader() {
+  const { tripLocation, eventStartAndEndDates} = useTrip();
+  const formattedDates = formatDateRange(eventStartAndEndDates,  ' au ');
+
   return (
     <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-[var(--shadow)] flex items-center justify-between">
       <div className="flex items-center gap-2">
         <MapPin className="size-5 text-zinc-400" />
-        <span className="text-lg text-zinc-100">Florianopolis, Brasil</span>
+        <span className="text-lg text-zinc-100">{tripLocation}</span>
       </div>
       <div className="flex items-center gap-5">
         <div className="flex items-center gap-2">
           <Calendar className="size-5 text-zinc-400" />
-          <span className="text-lg text-zinc-200">17 à 23 de Agosto</span>
+          <span className="text-lg text-zinc-200">{formattedDates}</span>
         </div>
         <div className="w-px h-6 bg-zinc-600" />
 
--- frontend/src/routes/Root.tsx
+++ frontend/src/routes/Root.tsx
@@ -1,17 +1,13 @@
 import Footer from '../pages/Footer/Footer'
 import Header from '../pages/Header/Header'
 import Home from '../pages/Home/Home'
-import { TripProvider } from '../context/TripContext'
 
 export default function App() {
   return (
     <div className="h-screen flex items-center justify-center bg-[var(--bgImage)] bg-no-repeat bg-center">
       <div className="max-w-3xl mx-auto px-6 text-center space-y-10 ">
         <Header />
-        <TripProvider>
-          <Home />
-        </TripProvider>
-
+        <Home />
         <Footer />
       </div>
     </div>
